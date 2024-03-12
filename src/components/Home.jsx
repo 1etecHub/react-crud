128da666675e7aad136c2a82131a77e98ba00a6d
Ebenezer <ebenirene5@gmail.com>
2024-03-12 19:12:42 +0100

    fixed errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,8 @@ export const Home = () => {
 
   return (
     <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
-      <h1>List of Users</h1>
+      <h1 >REACT CRUD OPERATION</h1>
+      <h2>List of Users</h2>
       <div className='w-75 rounded bg-white border shadow p-4'>
         <div className='d-flex justify-content-end'>
           <Link to="/create" className='btn btn-success'>Add +</Link>
