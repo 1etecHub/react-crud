0a4623105d6e438b5e34ad04a8fc46a1d07d1184
Ebenezer <ebenirene5@gmail.com>
2024-03-12 14:32:00 +0100

    finished implementing the create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,31 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export const Create = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8000/data", values)
+      .then((res) => {
+        navigate('/')
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
-      <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
         <h1>Add a User</h1>
-        <form>
-          <div className='mb-2'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
             <label htmlFor="name">Name:</label>
-            <input type="text" name='name' className='form-control' placeholder='Enter Name' />
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
           </div>
 
-          <div className='mb-2'>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name='name' className='form-control' placeholder='Enter Name' />
+          <div className="mb-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email Address"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
           </div>
 
-          <div className='mb-3'>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name='name' className='form-control' placeholder='Enter Name' />
+          <div className="mb-3">
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="text"
+              name="phone"
+              className="form-control"
+              placeholder="Enter Phone Number"
+              onChange={(e) => setValues({ ...values, phone: e.target.value })}
+            />
           </div>
-           
-          <button className='btn btn-success'>Submit</button>
-          <Link to='/' className='btn btn-primary ms-3'>Back</Link>
+
+          <button className="btn btn-success">Submit</button>
+          <Link to="/" className="btn btn-primary ms-3">
+            Back
+          </Link>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
